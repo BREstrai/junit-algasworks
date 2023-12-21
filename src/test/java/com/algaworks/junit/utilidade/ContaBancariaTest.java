3e0a9031b8b7c24a755f196ea72934f6cd06954d
Bruno <brunoestrai@gmail.com>
2023-12-21 18:43:26 -0300

    Testes Unitarios - Desafio do Banco

---
+++ src/test/java/com/algaworks/junit/utilidade/ContaBancariaTest.java
@@ -0,0 +1,78 @@
+package com.algaworks.junit.utilidade;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ContaBancariaTest {
+
+    @Test
+    void deveRetornarSaldoNaoNull() {
+        ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(100));
+
+        assertEquals(100, contaBancaria.saldo().intValue(), "Deve retornar 100");
+    }
+
+    @Test
+    void deveAguardarExceptionSeDepositarNull() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(100));
+            contaBancaria.deposito(null);
+        });
+
+        assertEquals("Valor inválido", exception.getMessage(), "Deve retornar 'Valor inválido'");
+    }
+
+    @Test
+    void deveAguardarExceptionSeDepositarMenosQueZero() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(100));
+            contaBancaria.deposito(BigDecimal.ZERO);
+        });
+
+        assertEquals("Valor inválido", exception.getMessage(), "Deve retornar 'Valor inválido'");
+    }
+
+    @Test
+    void deveAdicionarValorAoSaldo() {
+        ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(100));
+
+        contaBancaria.deposito(new BigDecimal(50));
+
+        assertEquals(150, contaBancaria.saldo().intValue(), "Deve retornar 150");
+    }
+
+    @Test
+    void deveAguardarExceptionSeSacarNull() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(100));
+            contaBancaria.saque(null);
+        });
+
+        assertEquals("Valor inválido", exception.getMessage(), "Deve retornar 'Valor inválido'");
+    }
+
+    @Test
+    void deveAguardarExceptionSeSacarMenosQueZero() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(100));
+            contaBancaria.saque(BigDecimal.ZERO);
+        });
+
+        assertEquals("Valor inválido", exception.getMessage(), "Deve retornar 'Valor inválido'");
+    }
+
+    @Test
+    void deveAguardarExceptionSeSacarMaisQueSaldo() {
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> {
+            ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(100));
+            contaBancaria.saque(new BigDecimal(150));
+        });
+
+        assertEquals("Saldo insuficiente", exception.getMessage(), "Deve retornar 'Saldo insuficiente'");
+    }
+
+}
